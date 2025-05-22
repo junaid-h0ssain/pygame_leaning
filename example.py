b6e8b07064ffa30aa060bda501784d2f258fda0d
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-05-22 22:16:34 +0600

    donno what i did

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 # Example file showing a circle moving on screen
 import pygame
+import random
+import time
 
 # pygame setup
 pygame.init()
@@ -7,9 +9,22 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-speed = 3000
+speed = 200
+bug_speed = 200
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 1.5)
+bug_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 3.5)
+playerImg = pygame.image.load('assets/rocket.png')
+bugImg = pygame.image.load('assets/bug.png')
+# player_x_pos = 400
+# player_y_pos = 400
+bug_movex = bug_pos.x
+bug_movey = bug_pos.y
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+def player(xpos,ypos):
+    screen.blit(playerImg,(xpos,ypos) )
+
+def bug(xpos,ypos):
+    screen.blit(bugImg,(xpos,ypos) )    
 
 while running:
     # poll for events
@@ -19,19 +34,47 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+    screen.fill("purple")
 
-    pygame.draw.circle(screen, "yellow", player_pos, 40)
+    player(player_pos.x,player_pos.y)
+    #bug(bug_pos.x,bug_pos.y)
+    #bug(xpos_bug,ypos_bug)
+    #bug_movement()
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= speed * dt
+        if player_pos.y >0:
+            player_pos.y -= speed * dt
     if keys[pygame.K_s]:
-        player_pos.y += speed * dt
+        if player_pos.y <620:
+            player_pos.y += speed * dt
     if keys[pygame.K_a]:
-        player_pos.x -= speed * dt
+        if player_pos.x >0:
+            player_pos.x -= speed * dt
     if keys[pygame.K_d]:
-        player_pos.x += speed * dt
+        if player_pos.x <1220:
+            player_pos.x += speed * dt 
+   
+    if bug_pos.y >=0 and bug_pos.y >=bug_movey:
+        bug_pos.y -= bug_speed * dt
+        bug_movey-=1
+    else: #bug_pos.y <=620 and bug_pos.y <=bug_movey:
+        bug_pos.y += bug_speed * dt
+        bug_movey-=1
+    if bug_pos.x >=0 and bug_pos.x >=bug_movex:
+        bug_pos.x -= bug_speed * dt
+        bug_movex-=1
+    else: #bug_pos.x <=1220 and bug_pos.x <=bug_movex:
+        bug_pos.x += bug_speed * dt
+        bug_movex-=1
+    
+    if bug_movex<=0 or bug_movey<=0:
+        bug_movex = random.randint(0,1200)
+        bug_movey = random.randint(0,700)
+
+    print(bug_movex,bug_movey)
+
+    
     
 
     # flip() the display to put your work on screen
@@ -40,6 +83,6 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(144) / 1000
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
