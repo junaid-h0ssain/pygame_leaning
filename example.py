f0cdf9573aef42c62b4212fd6775e9ce25f7d2c1
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-05-24 18:29:39 +0600

    made enemy move a function

--- example.py
+++ example.py
@@ -9,8 +9,8 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-speed = 200
-bug_speed = 100
+speed = 1000
+bug_speed = 200
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 1.5)
 bug_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 3.5)
 playerImg = pygame.image.load('assets/rocket.png')
@@ -23,8 +23,27 @@ i = 0
 def player(xpos,ypos):
     screen.blit(playerImg,(xpos,ypos) )
 
-def bug(xpos,ypos):
-    screen.blit(bugImg,(xpos,ypos) )    
+def bug(xpos,ypos,ranmin):
+    screen.blit(bugImg,(xpos,ypos) )
+    global bug_movex
+    global bug_movey
+    if bug_pos.x !=bug_movex and bug_pos.y != bug_movey:
+        if bug_pos.x >=bug_movex:
+            bug_pos.x -= bug_speed * dt
+        else:
+            bug_pos.x += bug_speed * dt
+        # if bug_pos.y >bug_movey:
+        #     bug_pos.y -= bug_speed * dt
+        # else:
+        #     bug_pos.y += bug_speed * dt
+
+    if bug_movex<=0 or bug_movey<=0:
+        bug_movex = random.randint(0,1280)-ranmin
+        bug_movey = random.randint(0,720)+ranmin
+        print(bug_movex,bug_movey)
+    else:
+        bug_movex-=1
+        bug_movey-=1
 
 while running:
     # poll for events
@@ -37,7 +56,9 @@ while running:
     screen.fill("purple")
 
     player(player_pos.x,player_pos.y)
-    bug(bug_pos.x,bug_pos.y)
+    bug(bug_pos.x+150,bug_pos.y-50,500)
+    bug(bug_pos.x,bug_pos.y,100)
+    bug(bug_pos.x-100,bug_pos.y+100,150)
     #bug(xpos_bug,ypos_bug)
     #bug_movement()
 
@@ -46,7 +67,7 @@ while running:
         if player_pos.y >0:
             player_pos.y -= speed * dt
     if keys[pygame.K_s]:
-        if player_pos.y <620:
+        if player_pos.y <650:
             player_pos.y += speed * dt
     if keys[pygame.K_a]:
         if player_pos.x >0:
@@ -55,28 +76,19 @@ while running:
         if player_pos.x <1220:
             player_pos.x += speed * dt 
    
-    if bug_pos.y >bug_movey:
-        bug_pos.y -= bug_speed * dt
-        
-    if bug_pos.y <bug_movey:
-        bug_pos.y += bug_speed * dt
-        
-    if bug_pos.x >=bug_movex:
-        bug_pos.x -= bug_speed * dt
-        
-    if bug_pos.x <bug_movex:
-        bug_pos.x += bug_speed * dt
-        
-    
-    if bug_movex<=0 or bug_movey<=0:
-        bug_movex = random.randint(0,1200)
-        bug_movey = random.randint(0,700)
-    else:
-        bug_movex-=1
-        bug_movey-=1
+    # if bug_pos.y >bug_movey:
+    #     bug_pos.y -= bug_speed * dt
+    # if bug_pos.x >=bug_movex:
+    #     bug_pos.x -= bug_speed * dt
+    # if bug_pos.y <bug_movey:
+    #     bug_pos.y += bug_speed * dt
+    # if bug_pos.x <bug_movex:
+    #     bug_pos.x += bug_speed * dt
+
+   
 
     if ((bug_movex>=(player_pos.x-140)) and (bug_movex <=(player_pos.x+140))) and ((bug_movey>=(player_pos.y-140)) and (bug_movey <=(player_pos.y+140))):
-        print(f'Hit!!{i}')
+        #print(f'Hit!!{i}')
         i+=1
         
 
@@ -86,6 +98,6 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(240) / 1000
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
